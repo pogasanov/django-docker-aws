fb41f85251eb961801a702032e980d5d7ff2dd3e
pavel <pogasanov@gmail.com>
2018-07-21 22:30:27 +0700

    Add comments in configuration files

--- config/app/on-container-start.sh
+++ config/app/on-container-start.sh
@@ -1,3 +1,9 @@
+# Create migrations based on django models
 python manage.py makemigrations
+
+# Migrate created migrations to database
 python manage.py migrate
+
+# Start gunicorn server at port 8000 and keep an eye for app code changes
+# If changes occur, kill worker and start a new one
 gunicorn --reload myproject.wsgi:application -b 0.0.0.0:8000
