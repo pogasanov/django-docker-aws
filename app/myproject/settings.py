7996962e7fe35a1e65c6e68ec133b3f006ecfb3f
pavel <pogasanov@gmail.com>
2019-03-27 16:40:13 +0700

    separate django settings

--- app/myproject/settings.py
+++ app/myproject/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'g!@a2x+)1y$w_zo7q(vei!q90-tk_$si97%zfx%r^5_sw1o%sg'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 # Application definition
 
---
+++ app/myproject/settings_dev.py
@@ -0,0 +1,11 @@
+# Development settings - DO NOT USE THIS IN PRODUCTION
+from myproject.settings import *
+
+# some constant secret key
+SECRET_KEY = 'g!@a2x+)1y$w_zo7q(vei!q90-tk_$si97%zfx%r^5_sw1o%sg'
+
+# Debug mode turned on
+DEBUG = True
+
+# Allowing all hosts
+ALLOWED_HOSTS = ['*']
